f0ac4e1726727ab92ef26f3dd2be6535d6994185
Matthew MacPhail <2996230m@student.gla.ac.uk>
2026-01-26 20:14:44 +0000

    Chapter 3 Pre-test commit

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
