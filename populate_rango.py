e61587a1607e5a01dacccfc53f8f317f4f40f65f
Matthew MacPhail <2996230m@student.gla.ac.uk>
2026-02-05 12:20:00 +0000

    Chapter 6 successful test

--- populate_rango.py
+++ populate_rango.py
@@ -1,7 +1,7 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "tango_with_django_project.settings")
-
+import random
 import django
 django.setup()
 from rango.models import Category, Page
@@ -13,25 +13,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
+         'url': 'http://docs.python.org/3/tutorial/',
+         'views': random.randint(0,1023)},
         {'title': 'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views': random.randint(0,1023)},
         {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}]
+         'url': 'http://www.korokithakis.net/tutorials/python/',
+         'views': random.randint(0,1023)}]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': random.randint(0,1023)},
         {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
+         'url': 'http://www.djangorocks.com/',
+         'views': random.randint(0,1023)},
         {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}]
+         'url': 'http://www.tangowithdjango.com/',
+         'views': random.randint(0,1023)}]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views': random.randint(0,1023)},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'},
+         'url':'http://flask.pocoo.org',
+         'views': random.randint(0,1023)},
     ]
 
     cats = {'Python': {'pages':python_pages, 'views':128,'likes':64},
@@ -45,7 +53,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
